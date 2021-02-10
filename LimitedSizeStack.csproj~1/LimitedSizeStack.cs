201ae4a61a884b39bb71bc2f2f7ab5dc82b3e471
Shrrgnien <42436216+Shrrgnien@users.noreply.github.com>
2021-02-10 04:28:01 +0300

    Add files via upload

---
+++ LimitedSizeStack.csproj~1/LimitedSizeStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        LinkedList<T> stack = new LinkedList<T>();
+        int Limit { get; set; }
+
+        public LimitedSizeStack(int limit)
+        {
+            Limit = limit;
+        }
+
+        public void Push(T item)
+        {
+            if (Limit == stack.Count)
+                stack.RemoveFirst();
+            stack.AddLast(item);
+        }
+
+        public T Pop()
+        {
+            T result = stack.Last.Value;
+            stack.Remove(stack.Last);
+            return result;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return stack.Count;
+            }
+        }
+    }
+}
---
+++ LimitedSizeStack.csproj~1/ListModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public enum  ActionMade{
+            Pushed,
+            Poped
+        };
+
+        public List<TItem> Items { get; }
+        public int Limit;
+        public LimitedSizeStack<TItem> ItemsStack;
+        public LimitedSizeStack<ActionMade> ActionStack;
+        public LimitedSizeStack<int> IndexStack;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+            ItemsStack = new LimitedSizeStack<TItem>(limit);
+            ActionStack = new LimitedSizeStack<ActionMade>(limit);
+            IndexStack = new LimitedSizeStack<int>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            ItemsStack.Push(item);
+            ActionStack.Push(ActionMade.Pushed);
+            IndexStack.Push(Items.IndexOf(item));
+        }
+
+        public void RemoveItem(int index)
+        {
+            ItemsStack.Push(Items[index]);
+            ActionStack.Push(ActionMade.Poped);
+            IndexStack.Push(index);
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return ItemsStack.Count > 0;
+        }
+
+        public void Undo()
+        {
+            if (CanUndo())
+            {
+                var action = ActionStack.Pop();
+                switch (action)
+                {
+                    case ActionMade.Pushed:
+                        Items.Remove(ItemsStack.Pop());
+                        IndexStack.Pop();
+                        break;
+                    case ActionMade.Poped:
+                        Items.Insert(IndexStack.Pop(), ItemsStack.Pop());
+                        break;
+
+                }
+            }
+        }
+    }
+}
